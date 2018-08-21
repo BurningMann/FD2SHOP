7782794896cacaa51066a7b14987cf97b9a6e3dd
burtest <sannnnna@bk.ru>
2018-08-21 17:17:31 +0300

    third

--- www/src/js/script.js
+++ www/src/js/script.js
@@ -1,7 +1,52 @@
 'use strict';
 $(document).ready(function() {
 	switchToStateFromURLHash()
+	let div_menu = $(`<div class="div_menu">
+						<div class="div_navigation1">
+							<h1 class="h2_navigation1">Игровые компьютеры</h1>	
+							<ul class="ul_navigation1">
+							<li class="li_navigation1"  onclick="render('progaming')" >PROGAMING СЕРИЯ</li>
+							<li class="li_navigation1" onclick="render('perfomance')">PERFORMANCE СЕРИЯ</li>
+							<li class="li_navigation1" onclick="render('perfomanceX')">PERFORMANCE X</li>
+							<li class="li_navigation1" onclick="render('mono')">ИГРОВОЙ МОНОБЛОК</li>
+							<li class="li_navigation1" onclick="render('poweredByAMD')">POWERED BY AMD</li>
+							</ul>
+						</div>   
+					</div>
+					<div class="div_menu">
+						<div class="div_navigation1">
+							<h1 class="h2_navigation1">Игровое пространство</h1>	
+							<ul class="ul_navigation1">
+							<li class="li_navigation1">ИГРОВЫЕ СТОЛЫ</li>
+							<li class="li_navigation1">ИГРОВЫЕ КРЕСЛА</li>
+							</ul>
+						</div>   
+					</div>`);
+$("#menu").append(div_menu);
+let div_footer= $(`<div class="footer_box"></div>
+
+
+
+
+
+
+
+`)
+$("#footer").append(div_footer);
+$(".li_navigation1").click(function(){
+		console.log("gg")
+		$('.nav-toggle').toggleClass('active');
+		$("#menu").toggleClass('menu');
+			if ($("#menu").attr('class') == "") {
+			$("#menu").addClass("menuout");
+			setTimeout(function() {
+			$("#menu").removeClass("menuout");
+		}, 250);
+	};	
+	});
+
 });
+	
 $('.nav-toggle').click(function() {
 	$(this).toggleClass('active');
 	$("#menu").toggleClass('menu');
@@ -12,9 +57,11 @@ $('.nav-toggle').click(function() {
 		}, 250);
 	};
 });
+
 window.onhashchange = switchToStateFromURLHash;
 
 function switchToStateFromURLHash() {
+
 	var URLHash = window.location.hash;
 	var stateStr = URLHash.substr(1);
 	if (stateStr == "") {
@@ -56,28 +103,19 @@ function switchToStateFromURLHash() {
 		});		
 	}
 }
-let div = $(`<div class="div_menu">
-						<div class="div_navigation1">
-							<h1 class="h2_navigation1">Игровые компьютеры</h1>	
-							<ul class="ul_navigation1">
-							<li class="li_navigation1"  onclick="render('progaming')" >PROGAMING СЕРИЯ</li>
-							<li class="li_navigation1" onclick="render('perfomance')">PERFORMANCE СЕРИЯ</li>
-							<li class="li_navigation1" onclick="render('perfomanceX')">PERFORMANCE X</li>
-							<li class="li_navigation1" onclick="render('mono')">ИГРОВОЙ МОНОБЛОК</li>
-							<li class="li_navigation1" onclick="render('poweredByAMD')">POWERED BY AMD</li>
-							</ul>
-						</div>   
-					</div>
-					<div class="div_menu">
-						<div class="div_navigation1">
-							<h1 class="h2_navigation1">Игровое пространство</h1>	
-							<ul class="ul_navigation1">
-							<li class="li_navigation1">ИГРОВЫЕ СТОЛЫ</li>
-							<li class="li_navigation1">ИГРОВЫЕ КРЕСЛА</li>
-							</ul>
-						</div>   
-					</div>`);
-$("#menu").append(div);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -185,13 +223,6 @@ function Progaming(obj) {
 
 
 
-
-
-
-
-
-
-
 function ASUS(obj) {
 	let Progaming_page = $(`
 		<div id="container">
@@ -277,6 +308,21 @@ function ASUS(obj) {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function MONO(obj) {
 	let Progaming_page = $(`
 		<div id="container">
@@ -334,9 +380,128 @@ function MONO(obj) {
 function About(obj){
 	var URLHash = window.location.hash;
 	var stateStr = URLHash.substr(1);
-	console.log(stateStr.split(''))	
-	stateStr =stateStr.replace(/\s+/g, '%C2%');
-	console.log(stateStr)
+	console.log(obj[stateStr][0].image)
+	
+	let Progaming_page = $(`
+		<div id="container">
+			<div class="page_description">	
+				<div class="page_description_box_about" style="background-image:url('${obj[stateStr][0].image}');"></div>
+				<div class="page_description_box_about2">
+					<h1 class="about_title">${stateStr}</h1>
+					<br/>
+					<br/>
+					Краткие характеристики:
+					<br/>
+					<br/>
+					<ul class="about_product">
+						<li class="about_product_li"></li>
+					</ul>
+				
+				
+				
+				</div>
+			</div>
+		</div>
+		`)
+	$("#content").append(Progaming_page);
+	let accessories = []
+	for (let i = 0; i < obj[stateStr].length; i++) {
+			let keys = Object.keys(obj[stateStr][i]);
+			for (let x = 0; x < keys.length; x++) {
+				accessories.push(keys[x])
+			}
+		}
+	for (let i = 0; i < accessories.length; i++) {
+		if(accessories[i]!="image"){
+			let phase1 = obj[stateStr][i]
+			let li = $(`<li class="about_product_li">- ${accessories[i]}: ${phase1[accessories[i]]}</li>`);
+			$(".about_product").append(li);
+		}
+	}
+/*
+	for (let i = 0; i < obj.features.length; i++) {
+		let div = $(`<div class="features_box"><img src="${obj.features[i].image}"></div>`);
+		$(".features").append(div);
+	}
+	for (let x = 0; x < obj.goods.length; x++) {
+		let blockProducts = $(`<div class="product"><div class="product_name">${obj.goods[x].name}</div><div class="product_price">${obj.goods[x].price}</div></div>`);
+		$(".name_of_product").append(blockProducts);
+	}
+	let kkk = $(".product")[0];
+	$(kkk).addClass("active_product")
+	let texts = $(".active_product")[0].childNodes[0]
+	fill(obj[$(texts).text()])
+	for (let i = 0; i < $(".product").length; i++) {
+		$(".product")[i].onclick = function() {
+			if (!this.classList.contains('active_product')) {
+				$(".product_image").children()[0].remove()
+				let deleted_characteristics = $(".product_characteristics").children().length
+				for (let y = 0; y < deleted_characteristics; y++) {
+					$(".product_characteristics").children()[0].remove()
+				}
+				let deleted = $(".active_product")[0]
+				$(deleted).removeClass("active_product")
+				$(this).addClass('active_product');
+				let texts = $(".active_product")[0].childNodes[0]
+				return fill(obj[$(texts).text()])
+			};
+		}
+	}
+	$(".about_box").click(function(){
+		let texts = $(".active_product")[0].childNodes[0]
+		 texts=$(texts).text().replace(/\s+/g, '');
+		console.log(texts)
+		return render(texts)
+
+	})
+
+	function fill(productName) {
+		let accessories = []
+		for (let i = 0; i < productName.length; i++) {
+			let keys = Object.keys(productName[i]);
+			for (let x = 0; x < keys.length; x++) {
+				accessories.push(keys[x])
+			}
+		}
+		for (let i = 0; i < productName.length; i++) {
+			let keys = Object.keys(productName[i])
+			for (let x = 0; x < keys.length; x++) {
+				if (keys[x] == "image" && Object.keys(productName[i][keys[x]][0]).length != 0) {
+					let imageProduct = $(`<img src="${productName[i].image}">`);
+					$(".product_image").append(imageProduct);
+				}
+				if (accessories.indexOf(keys[x]) != -1 && Object.keys(productName[i][keys[x]][0]).length != 0 && keys[x] != "image") {
+					let div = $(`<div class="product_characteristics_box">
+								<h2>${productName[i][keys[x]][0].number}</h2>
+								<p>${productName[i][keys[x]][0].name1}</br>${productName[i][keys[x]][0].name2}</p>
+							</div>`)
+					$(".product_characteristics").append(div);
+				}
+			}
+		}
+	} */
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 }
